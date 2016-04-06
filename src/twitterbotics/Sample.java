80a39b84d63b30c13e9b6174974aa62736f2697c
Hamid Reza Ghaeini <ghaeini@acm.org>
2016-04-06 17:11:20 +0200

    Correct path

--- src/twitterbotics/Sample.java
+++ src/twitterbotics/Sample.java
@@ -2,8 +2,10 @@ package twitterbotics;
 
 public class Sample {
     public static void main (String[] args) {
-	String kdir=args[0];
-        KnowledgeBaseModule NOC          = new KnowledgeBaseModule(kdir + "Veale's The NOC List.txt", 0);
-        KnowledgeBaseModule CATEGORIES   = new KnowledgeBaseModule(kdir + "Veale's Category Hierarchy.txt", 0);
+    	String path = "D:/TSV Lists/";
+    	//path = path.replaceAll("\\", "/");
+    	KnowledgeBaseModule NOC          = new KnowledgeBaseModule(path + "Veale's The NOC List.txt", 0);
+        KnowledgeBaseModule CATEGORIES   = new KnowledgeBaseModule(path + "Veale's Category Hierarchy.txt", 0);
+        
     }
 }
