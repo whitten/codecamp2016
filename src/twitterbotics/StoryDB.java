2be8c52a711390a68f4da557e1d93be94c1aea3e
Kasia <k.bigaj@uj.edu.pl>
2016-04-07 12:18:05 +0200

    StoryDB stub

--- src/twitterbotics/StoryDB.java
+++ src/twitterbotics/StoryDB.java
@@ -1,5 +1,6 @@
 package twitterbotics;
 
+import java.util.Hashtable;
 import java.util.Random;
 import java.util.Vector;
 
@@ -16,18 +17,35 @@ public class StoryDB {
     private KnowledgeBaseModule NOC          = null;
     private KnowledgeBaseModule INIT         = null;
     private KnowledgeBaseModule ENDING       = null;
+    private KnowledgeBaseModule ANTONYMS	 = null;
+    private Hashtable NEG_QUALITIES 		 = null;
 
 
     public StoryDB(String kbDirectory) {
         knowledgeDir = kbDirectory;
 
         NOC = new KnowledgeBaseModule(knowledgeDir + "Veale's The NOC List.txt", 0);
+        ANTONYMS = new KnowledgeBaseModule(knowledgeDir + "antonyms.txt", 0);
+        NEG_QUALITIES = NOC.getInvertedField("Negative Talking Points");
         INIT = new KnowledgeBaseModule(knowledgeDir + "Veale's initial bookend actions.txt", 0);
         ENDING = new KnowledgeBaseModule(knowledgeDir + "Veale's closing bookend actions.txt", 0);
     }
 
-    public void generateDummyPlot () {
+    public int roll(int size) {
+        return DICE.nextInt(size);
+    }
+
+
+    public void generateAntagonists () {
         Vector exemplars = INIT.getKeyConcepts();
+        String exemplar = (String)exemplars.elementAt(roll(exemplars.size()));
+        Vector posQuals = NOC.getFieldValues("Positive Talking Points", exemplar);
+        String pos = (String)posQuals.elementAt(roll(posQuals.size()));
+        Vector opposites = ANTONYMS.getFieldValues("Antonym", pos);
+        if (opposites == null) return;
+        String opposite  = (String)opposites.elementAt(roll(opposites.size()));
+        Vector instances = (Vector)NEG_QUALITIES.get(opposite);
+        if (instances == null) return;
 
     }
 }
